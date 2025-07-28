c78d8a8821579c433c1fd0dbb13d5482a3049ad4
uvaw <akimovivan13@gmail.com>
2025-07-28 13:56:24 +0300

    Finish basic assignments

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 "use strict";
 
-const myLibrary = JSON.parse(localStorage.getItem("library")) || [];
+const storedBooks = JSON.parse(localStorage.getItem("library")) || [];
+const myLibrary = storedBooks.map(bookData => {
+    const book = new Book(
+        bookData.author,
+        bookData.title,
+        bookData.info,
+        bookData.pages,
+        bookData.read
+    );
+    book.id = bookData.id; // Preserve the ID
+    return book;
+});
 
 let libEl;
 
@@ -17,7 +28,7 @@ function Book(author, title, info, pages, read) {
     this.read = read;
     this.toggleReadStatus = function () {
         this.read = !this.read;
-    }
+    };
 }
 
 function addBookToLibrary(author, title, info, pages, read) {
@@ -41,13 +52,42 @@ function removeBookFromLibrary(id) {
 function renderLibrary() {
     libEl.innerHTML = "";
     for (const book of myLibrary) {
-        const el = document.createElement("li");
-        el.setAttribute('data-id', book.id);
-        el.innerText = book.title;
+        // const el = document.createElement("div");
+        // el.setAttribute('data-id', book.id);
+        //
+        // el.innerText = book.title;
+
+        const el = document.createElement("div");
+        el.setAttribute("data-id", book.id);
+        el.classList.add("book-card");
+
+        el.innerHTML = `
+            <div class="book-header">
+                <button onclick="removeBookFromLibrary('${book.id}')" class="trash-bin" aria-label="Delete Book">
+                    <svg class="trash-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff">
+                        <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/>
+                    </svg>
+                </button>
+                <h2 class="book-title">${book.title}</h2>
+                <p class="book-author">${book.author}</p>
+            </div>
+            <div class="book-body">
+                <p class="book-description">
+                    ${book.info}
+                </p>
+                <div class="book-meta">
+                    <span class="pages">${book.pages} pages</span>
+                    <span class="read-status ${book.read ? '' : 'not-read'}">${book.read ? 'read' : 'not read'}</span>
+                </div>
+            </div>
+        `
+
+
         libEl.appendChild(el);
 
-        el.addEventListener("click", () => {
-            return removeBookFromLibrary(book.id);
+        el.querySelector(".read-status").addEventListener("click", () => {
+            book.toggleReadStatus();
+            updateState();
         });
     }
 }
@@ -58,7 +98,7 @@ function updateState() {
 }
 
 // open modal by id
-function openModal(id) {
+function openModal() {
     document.getElementById('modal').classList.add('open');
     document.body.classList.add('jw-modal-open');
 }
@@ -74,13 +114,9 @@ window.addEventListener('load', function () {
 document.addEventListener("DOMContentLoaded", () => {
     libEl = document.getElementById("libraryList");
     const form = document.getElementById("addNewBookForm");
-    // addBookToLibrary("sas", "velik", "", 13, false);
-    // addBookToLibrary("sas", "velik", "", 13, false);
-    // addBookToLibrary("sas", "velik", "", 13, false);
 
     renderLibrary();
 
-    // close modals on background click
     document.addEventListener('click', event => {
         if (event.target.classList.contains('jw-modal')) {
             closeModal();
